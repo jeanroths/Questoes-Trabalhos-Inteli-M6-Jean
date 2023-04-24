df20603d652cbef2bd9e55d698ec648258a1c8e6
jeanroths <jean.machado@sou.inteli.edu.br>
2023-04-24 17:09:16 -0300

    Atualização comentários e README

---
+++ ponderada1/python-turtle1/venv/README.md
@@ -0,0 +1,13 @@
+# Atividade 1: Turtlesim: simulando um ambiente robótico integrado no ROS
+
+## Enunciado
+
+Crie um script em Python capaz de interagir com o nó de simulação do turtlesim e enviar mensagens nos tópicos que regem a locomoção da tartaruga principal. Utilize este script para reproduzir um desenho de sua autoria. Utilize a estrutura de dados que preferir para representar a “imagem” a ser desenhada. O uso de programação orientada a objetos é obrigatório.
+
+## Descrição da solução
+
+Este é um código que controla um robô tartaruga (turtle) em ROS (Robot Operating System). Ele cria um nó (node) chamado "turtle_controller" que publica comandos de movimento para a tartaruga no tópico "/turtle1/cmd_vel". A cada 1 segundo (definido pelo timer), o método "move_turtle" é chamado, que define os comandos de movimento (linear e angular) para a tartaruga.
+
+O código faz a tartaruga seguir um trajeto que forma um desenho. A variável "counter_" é usada para determinar qual comando de movimento deve ser executado em cada chamada da função "move_turtle". O valor da velocidade linear (linear.x) e angular (angular.z) são definidos para cada ponto do trajeto, causando a movimentação da tartaruga de acordo com as coordenadas especificadas.
+
+Por fim, o código usa a função "spin" do ROS para manter o nó em execução até que o programa seja encerrado.
--- ponderada1/python-turtle1/venv/Scripts/Turtle1.py
+++ ponderada1/python-turtle1/venv/Scripts/Turtle1.py
@@ -1,63 +1,77 @@
 #!/usr/bin/env python3
+# Importando bibliotecas necessárias
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
-
+#Classe TurtleController
 class TurtleController(Node):
     def __init__(self):
         super().__init__('turtle_controller')
         self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
         self.timer_ = self.create_timer(1, self.move_turtle)
-        self.twist_msg_ = Twist()
+        self.twist_msg_ = Twist() #mensagens que publicam velocidade linear e angular
         self.counter_ = 0
 
     def move_turtle(self):
         self.counter_ += 1
+        #Movimentação da tartaruga fazendo a primeira linha
         if self.counter_ == 1:
             self.twist_msg_.linear.x = 3.0
             self.twist_msg_.angular.z = 0.0
+          #Movimentação da tartaruga fazendo a primeira rotação
         if self.counter_ == 2:
             self.twist_msg_.linear.x = 0.0
             self.twist_msg_.angular.z = 2.0
+          #Movimentação da tartaruga fazendo a segunda linha
         if self.counter_ == 3:
              self.twist_msg_.linear.x = 3.0
              self.twist_msg_.angular.z = 0.0
+          #Movimentação da tartaruga fazendo a segunda rotação
         if self.counter_ == 4:
              self.twist_msg_.linear.x = 0.0
              self.twist_msg_.angular.z = 2.2
+          #Movimentação da tartaruga fazendo a terceira linha
         if self.counter_ == 5:
              self.twist_msg_.linear.x = 3.1
              self.twist_msg_.angular.z = 0.0
+          #Movimentação da tartaruga fazendo a terceira rotação
         if self.counter_ == 6:
              self.twist_msg_.linear.x = 0.0
              self.twist_msg_.angular.z = 2.05
+          #Movimentação da tartaruga fazendo o caminho até metade do lado do triângulo
         if self.counter_ == 7:
              self.twist_msg_.linear.x = 1.35
              self.twist_msg_.angular.z = 0.0
+          #Movimentação da tartaruga fazendo a circunferência
         if self.counter_ == 8:
              self.twist_msg_.linear.x = 0.5
              self.twist_msg_.angular.z = 0.6
+          #Movimentação da tartaruga fazendo parando para fazer a linha do meio
         if self.counter_ == 19:
              self.twist_msg_.linear.x = 0.0
              self.twist_msg_.angular.z = 0.0
         if self.counter_ == 20:
+          #Movimentação da tartaruga rodando para fazer a linha do meio
              self.twist_msg_.linear.x = 0.0
              self.twist_msg_.angular.z = 1.31
         if self.counter_ == 21:
+          #Movimentação da tartaruga fazendo a linha do meio
              self.twist_msg_.linear.x = 2.8
              self.twist_msg_.angular.z = 0.0    
         if self.counter_ == 22:
+          #Tartaruga parando
              self.twist_msg_.linear.x = 0.0
              self.twist_msg_.angular.z = 0.0         
         self.publisher_.publish(self.twist_msg_)
 
-def main(args=None):
+def main(args=None): #função principal
         rclpy.init(args=args)
         turtle_controller = TurtleController()
         rclpy.spin(turtle_controller)
         turtle_controller.destroy_node()
         rclpy.shutdown()
 
+#Execução do programa
 if __name__ == '__main__':
         main()
